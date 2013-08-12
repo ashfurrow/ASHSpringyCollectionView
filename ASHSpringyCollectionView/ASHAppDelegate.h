4c12182d44cc80c1055205b91934fcf06850d61b
Ash Furrow <ash@ashfurrow.com>
2013-08-12 14:49:42 -0400

    Initial Commit

---
+++ ASHSpringyCollectionView/ASHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASHAppDelegate.h
+//  ASHSpringyCollectionView
+//
+//  Created by Ash Furrow on 2013-08-12.
+//  Copyright (c) 2013 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ASHSpringyCollectionView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ASHSpringyCollectionView
+//
+//  Created by Ash Furrow on 2013-08-12.
+//  Copyright (c) 2013 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASHAppDelegate class]));
+    }
+}
